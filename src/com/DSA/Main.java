47b8dab03a66de7bed71bf819977f0c9e6d71458
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-02-06 23:29:02 -0800

    DSA Array

--- src/com/DSA/Main.java
+++ src/com/DSA/Main.java
@@ -1,8 +1,18 @@
 package com.DSA;
-
+import java.util.Scanner;
 public class Main {
     //*ARRAY*
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args){
+        //Syntax: dataType[] variable_name=new dataType[size]
+        Scanner sc=new Scanner(System.in);
+        int[] number=new int[3];
+        //reading an array
+        for(int i=0;i<number.length;i++) {
+            number[i]=sc.nextInt();
+        }
+        //printing an array
+        for(int i=0;i<number.length;i++) {
+            System.out.println(number[i]);
+        }
     }
 }
