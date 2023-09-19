05941a18899636bf510df33acfd86ca548810c36
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-09-19 11:15:42 +0530

    DSA Array leetcode

---
+++ src/com/DSA/sort/FirstMissingPositive.java
@@ -0,0 +1,33 @@
+package com.DSA.sort;
+
+import java.util.Arrays;
+
+//leetcode HARD 41
+public class FirstMissingPositive {
+    public static void main(String[] args) {
+        int[] num = {7,8,9,11,12};
+        int ans = firstMissingPositive(num);
+        System.out.println(Arrays.toString(num));
+        System.out.println(ans);
+    }
+    public static int firstMissingPositive(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i]-1;
+            if (nums[i] > 0 && nums[i]<= nums.length && nums[i] != nums[correct])
+                swap(nums, i, correct);
+            else
+                i++;
+        }
+        for(int index=0; index< nums.length; index++){
+            if(index+1 != nums[index])
+                return index+1;
+        }
+        return nums.length+1;
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
---
+++ src/com/DSA/sort/SetMismatch.java
@@ -0,0 +1,31 @@
+package com.DSA.sort;
+
+import java.util.Arrays;
+
+class SetMismatch {
+    public static void main(String[] args) {
+        int[] num = {1,3,4,2,2};
+        int[] ans=findErrorNums(num);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] findErrorNums(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct])
+                swap(nums, i, correct);
+            else
+                i++;
+        }
+        for(int index=0; index<nums.length; index++){
+            if(nums[index] != index+1)
+                return new int[] {nums[index],index+1};
+        }
+        return new int[]{-1,-1};
+    }
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first];
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
