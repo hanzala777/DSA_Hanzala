c36d4038a52a54914443524e371bd1cfede4e819
Hanzala Ahmed <hanzhanzala@gmail.com>
2023-09-21 09:55:13 +0530

    DSA Array leetcode

---
+++ src/com/DSA/strings/StrictlyPalindromic.java
@@ -0,0 +1,25 @@
+package com.DSA.strings;
+
+public class StrictlyPalindromic {
+    public static void main(String[] args) {
+        int n=1;
+        System.out.println(isStrictlyPalindromic(n));
+    }
+    public static boolean isStrictlyPalindromic(int n) {
+        for(int i=2; i<=n-2;i++){
+            if(!isPalindrome(Integer.toString(n,i)))
+                return false;
+        }
+        return true;
+    }
+    static boolean isPalindrome(String str){
+        str=str.toLowerCase();
+        for(int i=0; i<=str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if(start != end)
+                return false;
+        }
+        return true;
+    }
+}
---
+++ src/com/DSA/strings/palindrome.java
@@ -0,0 +1,18 @@
+package com.DSA.strings;
+
+public class palindrome {
+    public static void main(String[] args) {
+        String a = "HanzalalaznaH";
+        System.out.println(isPalindrome(a));
+    }
+    static boolean isPalindrome(String str){
+        str=str.toLowerCase();
+        for(int i=0; i<=str.length()/2; i++){
+            char start = str.charAt(i);
+            char end = str.charAt(str.length()-1-i);
+            if(start != end)
+                return false;
+        }
+        return true;
+    }
+}
