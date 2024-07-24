6f2eea170547594c274ad04006e0fa2c1f3ba302
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-07-24 22:09:38 +0530

    Linked-List Re-Start

--- src/com/DSA/LinkedLists/LL.java
+++ src/com/DSA/LinkedLists/LL.java
@@ -1,51 +1,47 @@
 package com.DSA.LinkedLists;
 
+
 public class LL {
     private Node head;
     private Node tail;
     private int size;
-    public LL(){
-        this.size = 0;
-    }
-
-    public void addFirst(int val){
+    public void insertFirst(int val){
         Node node = new Node(val);
         node.next = head;
         head = node;
 
-        if (tail == null){
+        if(tail == null){
             tail = head;
         }
         size += 1;
     }
-    public void addLast(int val){
+    public void insertLast(int val){
         if(tail == null){
-            addFirst(val);
-            return;
+            insertFirst(val);
         }
-        Node node = new Node(val);
+        Node node =  new Node(val);
         tail.next = node;
         tail = node;
         size += 1;
     }
-    public void add(int val, int index){
+    public void insert(int val, int index){
         if(index == 0){
-            addFirst(val);
+            insertFirst(val);
             return;
         }
         if(index == size){
-            addLast(val);
+            insertLast(val);
             return;
         }
         Node temp = head;
-        for (int i = 1; i < index; i++){
+        for(int i = 0; i < index-1; i++) {
             temp = temp.next;
         }
         Node node = new Node(val, temp.next);
         temp.next = node;
         size += 1;
     }
-    public int deleteFirst(){
+    public int removeFirst(){
         int val = head.value;
         head = head.next;
         if(head == null){
@@ -54,68 +50,65 @@ public class LL {
         size -= 1;
         return val;
     }
-    public int deleteLast(){
-        if (size <= 1){
-            return deleteFirst();
+
+    public int removeLast(){
+        if(size <= 1){
+            return removeFirst();
         }
-        Node secondLast = get(size-2);
+        Node second_last = get(size-2);
         int val = tail.value;
-        tail = secondLast;
+        tail = second_last;
         tail.next = null;
         return val;
     }
-    public int delete(int index){
-        if (index == 0){
-            return deleteFirst();
+
+    public int remove(int index){
+        if(index == 0){
+            return removeFirst();
         }
-        if (index == size - 1){
-            return deleteLast();
+        if(index == size-1){
+            return removeLast();
         }
-        Node prev = get(index - 1);
+        Node prev = get(index-1);
         int val = prev.next.value;
-
         prev.next = prev.next.next;
-
         return val;
     }
-    public Node get(int index) {
+
+    public Node get(int index){
         Node node = head;
-        for (int i = 0; i < index; i++) {
+        for(int i = 0; i < index; i++){
             node = node.next;
         }
         return node;
     }
-    public Node find(int value) {
-    Node node = head;
-    int index = 0;
-        while (node != null) {
-            if (node.value == value) {
-                System.out.println("Node found at index: " + index);
+    public Node search(int value){
+        Node node = head;
+        int index = 0;
+        while(node != null){
+            if(node.value == value){
+                System.out.println("Found at index " + index);
                 return node;
             }
             node = node.next;
-            index++;
+            index += 1;
         }
-        System.out.println("Node not found for value: " + value);
+        System.out.println("Not found");
         return null;
     }
-
     public void Display(){
         Node temp = head;
         System.out.print("START -> ");
-        while(temp != null){
+        while (temp != null){
             System.out.print(temp.value + " -> ");
             temp = temp.next;
         }
-        System.out.println("END");
+        System.out.print("END" + "\n");
     }
     private class Node{
         private int value;
         private Node next;
-
-        public Node(int value){
-            this.value = value;
-        }
+        public Node(int value){ this.value = value; }
         public Node(int value, Node next){
             this.value = value;
             this.next = next;
--- src/com/DSA/LinkedLists/LLI.java
+++ src/com/DSA/LinkedLists/LLI.java
@@ -1,34 +1,18 @@
 package com.DSA.LinkedLists;
 
 public class LLI {
-    public static void main(String[] args){
-        LL list=new LL();
-        list.addFirst(5);
-        list.addFirst(1);
-        list.addLast(2);
-        list.add(4, 2);
-        list.addLast(2);
-        list.add(4, 4);
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(6);
+        list.insertLast(3);
+        list.insertLast(2);
+        list.insertLast(4);
+        list.insert(1,3);
+        list.search(1);
         list.Display();
-        System.out.println(list.deleteFirst());
-        System.out.println(list.deleteLast());
-        System.out.println(list.delete(3));
+        System.out.println(list.remove(2));
+        System.out.println(list.removeFirst());
+        System.out.println(list.removeLast());
         list.Display();
-        list.find(4);
     }
 }
-/*Runtime complexity:
-      Lookup:
-         by value: O(n)
-         by Index: O(n)
-
-      Insert:
-         At the end      : O(1)
-         At the Beginning: O(1)
-         In the middle   : O(n)
-
-      Delete:
-         At the end      : O(1)
-         At the Beginning: O(n)
-         At the Middle   : O(n)
- */
