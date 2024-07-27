683be93744da96f88a336f103c30242c0fbac8af
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-07-27 13:37:04 +0530

    Linked-List problems

--- src/com/DSA/LinkedLists/CycleI.java
+++ src/com/DSA/LinkedLists/CycleI.java
@@ -6,6 +6,7 @@ public class CycleI {
         list.insertFirst(3);
         list.insertFirst(2);
         list.insertFirst(1);
+        list.insertFirst(3);
         System.out.println(list.hasCycle(list.head));
     }
 }
