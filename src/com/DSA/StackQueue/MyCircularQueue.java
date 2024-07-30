5ca4379f90a88fcea25244288a2c7fe63a367e17
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-07-30 21:48:25 +0530

    Circular Queue

---
+++ src/com/DSA/StackQueue/MyCircularQueue.java
@@ -0,0 +1,81 @@
+package com.DSA.StackQueue;
+
+class MyCircularQueue {
+    private final int[] queue;
+    private int front;
+    private int rear;
+
+    public MyCircularQueue(int k) {
+        queue = new int[k];
+        front = -1;
+        rear = -1;
+    }
+
+    public boolean enQueue(int value) {
+        if (isFull()) {
+            return false;
+        }
+        if (isEmpty()) {
+            front = 0;
+        }
+        rear = (rear + 1) % queue.length;
+        queue[rear] = value;
+        return true;
+    }
+
+    public boolean deQueue() {
+        if (isEmpty()) {
+            return false;
+        }
+        if (front == rear) { // Only one element left
+            front = -1;
+            rear = -1;
+        } else {
+            front = (front + 1) % queue.length;
+        }
+        return true;
+    }
+
+    public int Front() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return queue[front];
+    }
+
+    public int Rear() {
+        if (isEmpty()) {
+            return -1;
+        }
+        return queue[rear];
+    }
+
+    public boolean isEmpty() {
+        return front == -1;
+    }
+
+    public boolean isFull() {
+        return (rear + 1) % queue.length == front;
+    }
+    public static void main(String[] args) {
+        //Your MyCircularQueue object will be instantiated and called as such:
+        MyCircularQueue obj = new MyCircularQueue(5);
+        boolean param_11 = obj.enQueue(6);
+        boolean param_12 = obj.enQueue(26);
+        boolean param_13 = obj.enQueue(53);
+        boolean param_2 = obj.deQueue();
+        int param_3 = obj.Front();
+        int param_4 = obj.Rear();
+        boolean param_5 = obj.isEmpty();
+        boolean param_6 = obj.isFull();
+        System.out.println(param_11);
+        System.out.println(param_12);
+        System.out.println(param_13);
+        System.out.println(param_2);
+        System.out.println(param_3);
+        System.out.println(param_4);
+        System.out.println(param_5);
+        System.out.println(param_6);
+    }
+}
+
