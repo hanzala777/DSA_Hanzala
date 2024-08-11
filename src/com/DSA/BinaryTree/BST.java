111a2795dea602f19c4f6e55b7dd10e0fa894efd
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-08-11 13:13:04 +0530

    Binary tree Binary Search Tree

---
+++ src/com/DSA/BinaryTree/BST.java
@@ -0,0 +1,85 @@
+package com.DSA.BinaryTree;
+
+public class BST {
+    public static class Node {
+        int value;
+        Node left;
+        Node right;
+        int height;
+        public Node(int value){
+            this.value = value;
+        }
+        public int getValue() {
+            return value;
+        }
+    }
+    private Node root;
+    public BST(){}
+    public int height(Node node){
+        if (node == null){
+            return -1;
+        }
+        return node.height;
+    }
+
+    public boolean isEmpty(){
+        return root == null;
+    }
+
+    public void insert(int value){
+        root = insert(value, root);
+    }
+    private Node insert(int value, Node node){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.value){
+            node.left = insert(value, node.left);
+        }
+        if(value > node.value){
+            node.right = insert(value, node.right);
+        }
+
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+
+        return node;
+    }
+
+    public void populate(int[] arr){
+        for (int i : arr) {
+            this.insert(i);
+        }
+    }
+
+    public boolean balanced(){
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node){
+        if(node == null){
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public void display() {
+        display("Root Node : ", root);
+    }
+
+    private void display(String details, Node node){
+        if (node == null){
+            return;
+        }
+        System.out.println(details + node.getValue());
+        display("Left child of " + node.getValue() + " : ", node.left);
+        display("right child of " + node.getValue() + " : ", node.right );
+    }
+
+    public static void main(String[] args) {
+        BST bst = new BST();
+        bst.populate(new int[]{5, 3, 2, 4, 7, 6, 8});
+        bst.display();
+        System.out.println(bst.balanced());
+    }
+}
