3799f9f8fa6dab236fe90ac8d5c5823179317f52
Hanzala Ahmed <hanzhanzala@gmail.com>
2024-08-24 22:37:03 +0530

    Pattern4

--- src/com/DSA/Patterns/Pattern4.java
+++ src/com/DSA/Patterns/Pattern4.java
@@ -9,6 +9,7 @@ public class Pattern4 {
 //        1 2
 //        1 2 3
 //        1 2 3 4
+//        1 2 3 4 5
         for(int row = 0; row < n; row++){
             for(int col = 0; col <= row; col++){
                 System.out.print(col+1 + " ");
